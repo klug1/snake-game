ae107125ae492649ba2389fe134ae853a7887d6c
Patrick Almeida <patrick.automacao1@gmail.com>
2023-02-15 14:00:05 -0300

    Criar snake

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let context = canvas.getContext("2d");
 
 let box = 32;
 
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 function criarBG() {
     context.fillStyle = "lightgreen";
 
@@ -12,4 +18,12 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function createSnake() {
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+createSnake();
